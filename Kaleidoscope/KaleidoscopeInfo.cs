8c5c195caa94d94d5709062d33eaab591368b5c1
sqamuell <quietmilkshake@gmail.com>
2022-12-12 13:00:53 +0100

    Add project files.

---
+++ Kaleidoscope/KaleidoscopeInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace Kaleidoscope
+{
+    public class KaleidoscopeInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Kaleidoscope";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("e1916400-7369-4ae3-9b70-16ff35630dda");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
